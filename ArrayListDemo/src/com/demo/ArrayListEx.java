d29f927f733595db7b5eb4eaaae7f2b46f0bc87f
rutu987github <email@example.com>
2022-10-15 09:58:16 +0530

    ArrayList Program is added

---
+++ ArrayListDemo/src/com/demo/ArrayListEx.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayListEx {
+
+	public static void main(String[] args) {
+
+		List<String> list=Arrays.asList("Rutuja","Supriya","Komal","Trupti");
+		
+		System.out.println(list);
+	}
+
+}
